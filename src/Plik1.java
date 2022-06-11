ff0b9b6916a7b605122bd19b74b4b9c91e2f5105
Krzysztof <kiwanicki503@gmail.com>
2022-06-11 16:26:38 +0200

    Czwarty commit

--- src/Plik1.java
+++ src/Plik1.java
@@ -3,5 +3,6 @@ public class Plik1 {
     public static void main(String[] args) {
         System.out.println("Hello, cruel world!");
         System.out.println("Welcome to Hell");
+        System.out.println("Hot in here");
     }
 }
--- src/Plik2.java
+++ src/Plik2.java
@@ -4,6 +4,6 @@ public class Plik2 {
         System.out.println("Goodbye, cruel world!");
         System.out.println("-------------------------");
         System.out.println("Remember me for centuries");
-        System.out.println("Save me");
+        System.out.println("Save me right now");
     }
 }
