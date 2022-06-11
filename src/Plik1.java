86b380386487c7272011079094db2e02b15b7bac
Krzysztof <kiwanicki503@gmail.com>
2022-06-11 16:19:46 +0200

    Drugi commit

--- src/Plik1.java
+++ src/Plik1.java
@@ -2,5 +2,6 @@ public class Plik1 {
 
     public static void main(String[] args) {
         System.out.println("Hello, cruel world!");
+        System.out.println("Welcome to Hell");
     }
 }
--- src/Plik2.java
+++ src/Plik2.java
@@ -2,5 +2,7 @@ public class Plik2 {
 
     public static void main(String[] args) {
         System.out.println("Goodbye, cruel world!");
+        System.out.println("-------------------------");
+        System.out.println("Remember me for centuries");
     }
 }
